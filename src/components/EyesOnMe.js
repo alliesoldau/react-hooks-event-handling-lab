ee02a51aec66f762e75c096a8965ca93706cfe9c
Allie Soldau <alliesoldau@gmail.com>
2022-11-21 11:02:24 -0500

    completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+function EyesOnMe() {
+    function focusFunction() {
+        console.log('Good!')
+    }
+    function blurFunction() {
+        console.log('Hey! Eyes on me!')
+    }
+    
+    return (
+        <button onFocus={(focusFunction)} onBlur={(blurFunction)}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe
